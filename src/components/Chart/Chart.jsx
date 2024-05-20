e2bd8221948a5813d49787f4f7d40e33195b37f2
Artem <a.g.sulimko@gmail.com>
2024-05-20 12:46:04 +0200

    add import ChartJS from "chart.js/auto" in Chart.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
+import ChartJS from "chart.js/auto";
 
 const Chart = ({ data }) => {
   const chartData = {
-    labels: ["Amount of registrations"], // Метка для оси X
+    labels: ["Amount of registrations"],
     datasets: [
       {
         label: "Number of Participants",
         data: [data],
-        backgroundColor: "rgba(85, 137, 235, 0.5)",
-        borderColor: "#b32727",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
         borderWidth: 1,
       },
     ],
@@ -21,7 +22,7 @@ const Chart = ({ data }) => {
 export default Chart;
 
 // ============================================
-// lineal Chart
+// Slineal Chart
 // import React from "react";
 // import { Line } from "react-chartjs-2";
 // import {
